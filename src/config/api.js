0324470e46b72245883c4c0befa8914e0ed4377c
richardcreatech <richardcreatech@gmail.com>
2026-09-01 10:38:35 +0100

    changed the url to a server for testing puroses

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "https://posted-numeral-eldest.ngrok-free.dev";
 
 export const apiFetch = (url, options = {}) =>
   fetch(url, {
