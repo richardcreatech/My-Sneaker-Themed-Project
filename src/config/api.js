1ef8c3e19153dd2656e14b7bedf157a399d30af6
richardcreatech <richardcreatech@gmail.com>
2026-09-01 14:12:26 +0100

    changed the url to a server to the deployed server

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://my-sneaker-backend.onrender.com";
+export const API_BASE_URL = "https://my-sneaker-backend.onrender.com";
 
 export const apiFetch = (url, options = {}) =>
   fetch(url, {
